254a5f80d25e665c300e7c9696a51c9bf42b7696
Michael Godfrey <m.godfrey59@gmail.com>
2023-01-23 22:51:28 +1200

    added sign in modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,31 +1,38 @@
 import React from 'react';
 
-const Modal = ({ isModalOpen, closeModal }) => {
-
-    return (
-      <div
-        className={`${
-          isModalOpen ? "modal-overlay show-modal" : "modal-overlay"
-        }`}
-      >
-        <div className="modal-container">
-          {/* <form>
-            <p>Register</p>
-            <input
-              type="email"
-              placeholder="email"
-              onChange={(event) => {
-                setRegisterEmail(event.target.value);
-              }}
-            />
-            <input
-              placeholder="password"
-              onChange={(event) => {
-                setRegisterPassword(event.target.value);
-              }}
-            />
-            <button onClick={register}>Sign up</button>
-            <p>Sign in</p>
+const Modal = ({
+  isModalOpen,
+  closeModal,
+  setRegisterEmail,
+  setRegisterPassword,
+  register,
+  setLoginEmail,
+  setLoginPassword,
+  login,
+}) => {
+  return (
+    <div
+      className={`${
+        isModalOpen ? "modal-overlay show-modal" : "modal-overlay"
+      }`}
+    >
+      <div className="modal-container">
+        <div className="modal-tab">
+          <input
+            className="radio"
+            id="tab1"
+            name="modal-tab"
+            type="radio"
+            checked
+          />
+          <label for="tab1">Log in</label>
+          <div className="modal-content">
+            <button
+              className="login-with-google-btn"
+              // onClick={signInWithGoogle}
+            >
+              Continue with Google
+            </button>
             <input
               type="email"
               placeholder="email"
@@ -41,13 +48,46 @@ const Modal = ({ isModalOpen, closeModal }) => {
               }}
             />
             <button onClick={login}>Login</button>
-          </form> */}
-          <button className="close-modal-btn" onClick={closeModal}>
-            close
-          </button>
+          </div>
         </div>
+        <div className="modal-tab">
+          <input className="radio" id="tab2" name="modal-tab" type="radio" />
+          <label for="tab2">Sign up</label>
+          <div className="modal-content">
+            <button
+              className="login-with-google-btn"
+              // onClick={signInWithGoogle}
+            >
+              Continue with Google
+            </button>
+            <input
+              type="email"
+              placeholder="email"
+              onChange={(event) => {
+                setRegisterEmail(event.target.value);
+              }}
+            />
+            <input
+              type="password"
+              placeholder="password"
+              onChange={(event) => {
+                setRegisterPassword(event.target.value);
+              }}
+            />
+            <input
+              type="password"
+              placeholder="confirm password"
+            />
+            <button onClick={register}>Sign up</button>
+          </div>
+        </div>
+
+        <button className="close-modal-btn" onClick={closeModal}>
+          close
+        </button>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Modal
